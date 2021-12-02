eb6077cb242eee525252d98059bcb9dbce311e2a
Tim Pierce <pierce@adobe.com>
2021-12-02 17:27:12 -0500

    Day 2

---
+++ Day_02/part_02.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+class Submarine(object):
+
+    def __init__(self):
+        self.horiz = 0
+        self.depth = 0
+        self.aim = 0
+
+    def forward(self, x):
+        self.horiz += x
+        self.depth += self.aim * x
+
+    def down(self, x):
+        self.aim += x
+
+    def up(self, x):
+        self.aim -= x
+
+
+def read_input(filename="input.txt"):
+    with open(filename, "r") as f:
+        data = f.readlines()
+    return data
+
+
+def main(filename="input.txt"):
+    directions = read_input(filename)
+    sub = Submarine()
+    for d in directions:
+        verb, distance = d.split()
+        if verb == "forward":
+            sub.forward(int(distance))
+        elif verb == "down":
+            sub.down(int(distance))
+        elif verb == "up":
+            sub.up(int(distance))
+        else:
+            raise ValueError(f"unknown verb in line {d}")
+
+    return sub.horiz * sub.depth
+
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
